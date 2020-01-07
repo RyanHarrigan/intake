42dcce899a4c38c55f52e8eb048457b7fa1695d3
Molly TM <mollyt@codeforamerica.org>
2020-01-07 10:28:00 -0800

    WIP upgrade to 2.0.13

--- intake/models/tag.py
+++ intake/models/tag.py
@@ -5,6 +5,7 @@ from taggit.models import TaggedItemBase
 class PurgedSubmissionTagLink(models.Model):
     """Placeholder for custom VIEW see intake migration 0067
     """
+
     class Meta:
         db_table = 'purged\".\"intake_submissiontaglink'
         managed = False
@@ -13,6 +14,7 @@ class PurgedSubmissionTagLink(models.Model):
 class PurgedTag(models.Model):
     """Placeholder for custom VIEW see intake migration 0067
     """
+
     class Meta:
         db_table = 'purged\".\"taggit_tag'
         managed = False
@@ -21,10 +23,14 @@ class PurgedTag(models.Model):
 class SubmissionTagLink(TaggedItemBase):
     added = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        'auth.User', null=True, on_delete=models.SET_NULL)
+        'auth.User',
+        models.SET_NULL,
+        null=True)
     content_object = models.ForeignKey(
-        'intake.FormSubmission', related_name='tag_links',
-        on_delete=models.CASCADE)
+        'intake.FormSubmission',
+        models.CASCADE,
+        related_name='tag_links',
+    )
 
     class Meta:
         ordering = ['added']
